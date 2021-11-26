a637759c11a505a85157fb949b9e047f1c5ad59a
Fabian <fabianbarraez@gmail.com>
2021-11-26 11:32:35 -0500

    feat: server mqttt response script

--- server/mqtt.py
+++ server/mqtt.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 from paho.mqtt.subscribeoptions import SubscribeOptions
 import time
+import os
+from urllib.parse import urlparse
 
 
 def on_message(client, userdata, message):
@@ -17,11 +19,21 @@ def on_message(client, userdata, message):
 broker_address = 'localhost'
 
 client = mqtt.Client("server", protocol=mqtt.MQTTv5)
-client.connect(broker_address)
+
+url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://localhost:18577')
+url = urlparse(url_str)
+# topic = url.path[1:] or 'test'
+
+# Connect
+client.username_pw_set(url.username, url.password)
+client.connect(url.hostname, url.port)
+
+# client.connect(broker_address)
 
 client.subscribe('common', options=SubscribeOptions(noLocal=True))
 
 client.on_message = on_message
+print("connected and waiting request")
 # client.loop_start()
 # time.sleep(10)  # wait
 # client.loop_stop()  # stop the loop
